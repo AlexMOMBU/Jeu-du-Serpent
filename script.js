48d92ba4b6e8b81cb4b717c76c51fb9c2ada59f2
alexMOMBU <alexMOMBU@gmail.com>
2024-02-15 22:48:08 +0100

    animation du canvas

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 //Tout ça c'est pour créer le canvas (l'espece de cadre noir  )
 window.onload = function()
 {
-    var canvas = document.createElement('canvas')
-    canvas.width =900;
-    canvas.height = 600;
-    canvas.style.border = '1px solid';
-    document.body.appendChild(canvas);
+    var canvas;
+    var ctx;
+    var delay=10;
+    var xCoord =0;
+    var yCoord =0;
 
+    init();
+
+    function init()
+    {
+        canvas = document.createElement('canvas');
+        canvas.width =900;
+        canvas.height = 600;
+        canvas.style.border = '1px solid';
+        document.body.appendChild(canvas);
+        ctx = canvas.getContext("2d");
+        refreshCanvas();
+
+        
+    
+
+       
+     }
+
+     function refreshCanvas()
+     {
 //Comment dessiner dans un canvas (en 2D pour notre cas)
-
-    var ctx = canvas.getContext('2d');
-    ctx.fillStyle ='#ff0000';
-    ctx.fillRect(30, 30, 100, 50 );
-
+        xCoord +=1;
+        yCoord +=1;
+        ctx.fillStyle ='#ff0000';
+        ctx.clearRect(0,0,canvas.width, canvas.height);
+        ctx.fillRect(xCoord, yCoord, 100, 50 );
+        setTimeout(refreshCanvas,delay);
+     }
 
 }
