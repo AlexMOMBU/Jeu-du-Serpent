bcc57882db7c165798b6463e679055582ab75a01
alexMOMBU <alexMOMBU@gmail.com>
2024-02-24 12:00:24 +0100

    Création du serpent

--- script.js
+++ script.js
@@ -1,39 +1,72 @@
 //Tout ça c'est pour créer le canvas (l'espece de cadre noir  )
 window.onload = function()
 {
-    var canvas;
+    var canvasWidth=900;
+    var canvasHeight=600;
+    var blockSize=30;
     var ctx;
-    var delay=10;
-    var xCoord =0;
-    var yCoord =0;
+    var delay=100;
+    
 
     init();
 
     function init()
     {
-        canvas = document.createElement('canvas');
-        canvas.width =900;
-        canvas.height = 600;
+        var canvas = document.createElement('canvas');
+        canvas.width =canvasWidth;
+        canvas.height = canvasHeight;
         canvas.style.border = '1px solid';
         document.body.appendChild(canvas);
         ctx = canvas.getContext("2d");
+        Snakee = new Snake( [[6,4], [5,4], [4,4]] );
         refreshCanvas();
-
-        
-    
-
+    }
        
-     }
-
+      
      function refreshCanvas()
      {
 //Comment dessiner dans un canvas (en 2D pour notre cas)
-        xCoord +=1;
-        yCoord +=1;
-        ctx.fillStyle ='#ff0000';
-        ctx.clearRect(0,0,canvas.width, canvas.height);
-        ctx.fillRect(xCoord, yCoord, 100, 50 );
+     
+        ctx.clearRect(0,0,canvasWidth, canvasHeight);
+        Snakee.advance();
+        Snakee.draw();
         setTimeout(refreshCanvas,delay);
      }
+
+     function drawBlock(ctx, position)
+     {
+        var x = position[0] * blockSize;
+        var y = position[1] * blockSize;
+        ctx.fillRect(x,y, blockSize, blockSize);
+
+     }
+
+ //On va créer une fonction constructeur ( prototype de notre serpent)
+     function Snake(body)
+     {
+        this.body= body;
+        this.draw = function()
+            {
+                ctx.save();
+                ctx.fillStyle ='#ff0000';
+                for(var i = 0; i< this.body.length; i++)
+                {
+                    drawBlock( ctx, this.body[i] );
+                }
+                ctx.restore();
+                   
+
+            };
+
+            this.advance = function()
+                {
+                    var nextPosition = this.body[0].slice();
+                    nextPosition[0] += 1;
+                    this.body.unshift(nextPosition);
+                    this.body.pop();
+                };
+            
+     } 
+
 
 }
